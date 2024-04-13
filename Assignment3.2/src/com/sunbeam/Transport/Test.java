238f1daaa56a459b8b1972c929339d4384e20c66
sunbeam <imran5692@gmail.com>
2024-04-13 16:18:46 +0530

    Assignment3-1 uploaded

---
+++ Assignment3.2/src/com/sunbeam/Transport/Test.java
@@ -0,0 +1,13 @@
+package com.sunbeam.Transport;
+import com.sunbeam.Transport.*;
+public class Test {
+
+	public static void main(String [] args)
+	{
+		TotalCost t1=new TotalCost();
+		t1.accept();
+		System.out.println("The total cost: "+t1.getTotal());
+	}
+	
+	
+}
